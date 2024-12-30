ef5968a1c830a7a835ed7b9629811ff8a6dd224b
hq <hq_cml@163.com>
2024-12-30 21:29:17 +0800

    集合

--- set.py
+++ set.py
@@ -56,6 +56,44 @@ def set2():
     print(s1 | s2) # 并集
     print(s1 - {1}) # 差集
 
+# 集合的更新、不可变集合
+def set3():
+    # 集合的更新
+    s1 = {1,2,3}
+    s1.update({2,3,4})
+    print(s1)
+
+    # 不可变集合
+    s2 = frozenset([1,2,3])
+    #s2.update({2, 3, 4}) # 报错，因为不可变
+
+    # 增加1个元素
+    s1.add(5)
+    print(s1)
+
+    # 删除一个元素
+    s1.remove(5) # 不存在则报错
+    print(s1)
+    s1.discard(5) # 不存在不报错
+    s1.pop() # 随机弹出1个元素
+    print(s1)
+
+    # 清空
+    s1.clear()
+
+# 可哈希：字典key和set，都必须是可hash的（也就是不可变值）
+def set4():
+    print(hash(1))
+    print(hash(1.0))
+    print(hash(1.1))
+
+    #通常来说，仅不可变的变量，才是可hash的
+    print(hash("hello"))
+    #print(hash([1,2])) 报错，因为[]是可变的
+    print(hash((1, 2)))
+
 
 #set1()
-set2()
+#set2()
+#set3()
+set4()
